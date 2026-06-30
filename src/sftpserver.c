4a5c897f089394a51956378c87fdfba1b3bace16
Jakub Jelen <jjelen@redhat.com>
2026-06-30 15:08:26 +0200

    sftp: Avoid handling repeated INIT messages
    Signed-off-by: Jakub Jelen <jjelen@redhat.com>
    Reviewed-by: Andreas Schneider <asn@cryptomilk.org>
    Reviewed-by: Pavol Žáčik <pzacik@redhat.com>
    Merge-Request: <https://gitlab.com/libssh/libssh-mirror/-/merge_requests/849>
    (cherry picked from commit 9c11f84b3cecb37b1bfdba6c3347ac5b023ecaf6)

--- src/sftpserver.c
+++ src/sftpserver.c
@@ -616,6 +616,15 @@ int sftp_reply_version(sftp_client_message client_msg)
     ssh_buffer reply;
     int rc;
 
+    /* The SSH_FXP_INIT can be received only once -- repeated initialization
+     * is not supported */
+    if (sftp->version > 0) {
+        ssh_set_error(sftp->session,
+                      SSH_FATAL,
+                      "Received duplicate INIT message");
+        return SSH_ERROR;
+    }
+
     SSH_LOG(SSH_LOG_PROTOCOL, "Sending version packet");
 
     /* from draft-spaghetti-sshm-filexfer-00 Section 4:
